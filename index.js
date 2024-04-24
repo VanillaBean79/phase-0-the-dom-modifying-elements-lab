bf6550736a8e186e4cbf17add8b55f279ff65684
Ruben Salazar <rubensalazar@rocketmail.com>
2024-04-24 16:33:26 -0500

    elements lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+main.remove('main')
+
+const newHeader = document.createElement("H1")
+
+newHeader.id = 'victory'
+
+newHeader.innerHTML = "Ruben Salazar is the champion"
+
